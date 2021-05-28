b7efcc5c4f56bd2ccfda71131ad5b47e1a34043f
BobTheSoftwareDeveloper <bob@jd.net.nz>
2021-05-29 01:03:12 +1200

    Add better clinic search filter

--- src/routes/clinics.js
+++ src/routes/clinics.js
@@ -17,6 +17,36 @@ clinicRoute.route("/clinic")
   .post(async (req, res) => {
   })
 
+clinicRoute.route("/clinic-search")
+  .post(async (req, res) => {
+    try {
+      const locationBound = req.body.bounds
+
+      let queryObj = {}
+      
+      /**
+       * Search by:
+       * - Location boundary (Passed in to POST)
+       * - Languages 
+       * - Price Range
+       * - Animal
+       * - Rating (Google Map data already exist)
+       */
+
+      if (locationBound) {
+        queryObj["location.lat"] = { $gte: locationBound.latStart, $lte: locationBound.latEnd }
+        queryObj["location.lng"] = { $gte: locationBound.lngStart, $lte: locationBound.lngEnd }
+      }
+
+      const result = await Vets.find(queryObj).select("name location vicinity").lean().exec()
+
+      return res.status(200).send(result)
+    } catch (err) {
+      console.error("POST /clinic-search error", err)
+      return res.status(500).send(err)
+    }
+  })
+
 export {
   clinicRoute
 }
